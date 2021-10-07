ca958b69746e674a4e7fe70aa52aa2b23ee5e82b
Luis Alberto Díaz Díaz <work_luis23_45diaz@protonmail.com>
2021-10-07 03:40:10 -0500

    corrected fast_test to point to example grammar

--- fast_test.py
+++ fast_test.py
@@ -1,28 +1,39 @@
 import sys 
-
+from typing import Dict
 import logging
 
 
 from lark import Lark, logger
-from lark.indenter import Indenter
+from lark.exceptions import UnexpectedToken, UnexpectedCharacters
+#from lark.indenter import Indenter
+from PyDayuri.indenter import Indenter,IndentType, IndenterError
 
-logger.setLevel(logging.DEBUG)
+#logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 
 class TreeIndenter(Indenter):
-    NL_type = '_NL'
-    OPEN_PAREN_types = []
-    CLOSE_PAREN_types = []
-    INDENT_type = '_INDENT'
-    DEDENT_type = '_DEDENT'
-    tab_len = 8
+    REGULAR_INDENT = ("_INDENT", "_DEDENT", "_NL")
+    INDENT_BLOCKS = {"_LET":(IndentType.at_next, "_IN", "_LET_SEPARATOR")}
+    PRODUCE_INDENT:Dict[str, IndentType] = dict()
 
-with open("src/grammar.lark") as f:
-  p = Lark(f,parser="lalr", postlex=TreeIndenter(), debug=True)
 
-#with open(sys.argv[1]) as f :
-#  f2 = f.read()
-#  parsed = p.parse(f2)
-#
-#print(f2)
-#print()
-#print(parsed.pretty())
+with open("examples/indenter.lark") as f:
+  p = Lark(f,parser="lalr", lexer="standard", postlex=TreeIndenter(), debug=True)
+
+with open(sys.argv[1]) as f :
+  f2 = f.read()
+  try :
+    parsed = p.parse(f2)
+    print(f2)
+    print()
+    print(parsed.pretty())
+  except IndenterError as e:
+      print(e.pre_msg)
+      print(UnexpectedToken.get_context(e.token,f2))
+      print(e.post_msg)
+  except UnexpectedToken as e:
+      print(e.get_context(f2))
+      print(e)
+  except UnexpectedCharacters as e:
+      print(e)
+
