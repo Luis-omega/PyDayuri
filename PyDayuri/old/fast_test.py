8966883d00ef21a5e032540d0c99878f1237d533
Luis Alberto Díaz Díaz <work_luis23_45diaz@protonmail.com>
2021-10-26 16:49:52 -0500

    Moved all old except test

---
+++ PyDayuri/old/fast_test.py
@@ -0,0 +1,39 @@
+import sys 
+from typing import Dict
+import logging
+
+
+from lark import Lark, logger
+from lark.exceptions import UnexpectedToken, UnexpectedCharacters
+#from lark.indenter import Indenter
+from PyDayuri.indenter import Indenter,IndentType, IndenterError
+
+#logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
+
+class TreeIndenter(Indenter):
+    REGULAR_INDENT = ("_INDENT", "_DEDENT", "_NL")
+    INDENT_BLOCKS = {"LET":(IndentType.at_next, "IN", "_LET_SEPARATOR")}
+    PRODUCE_INDENT:Dict[str, IndentType] = {"IN":IndentType.at_next}
+
+
+with open("examples/indenter.lark") as f:
+  p = Lark(f,parser="lalr", lexer="standard", postlex=TreeIndenter(), debug=True)
+
+with open(sys.argv[1]) as f :
+  f2 = f.read()
+  try :
+    parsed = p.parse(f2)
+    print(f2)
+    print()
+    print(parsed.pretty())
+  except IndenterError as e:
+      print(e.pre_msg)
+      print(UnexpectedToken.get_context(e.token,f2))
+      print(e.post_msg)
+  except UnexpectedToken as e:
+      print(e.get_context(f2))
+      print(e)
+  except UnexpectedCharacters as e:
+      print(e)
+
