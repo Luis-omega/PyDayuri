7c7c691a0e77715f8d54e303f523c220dae49f08
Luis Alberto Díaz Díaz <work_luis23_45diaz@protonmail.com>
2021-10-26 17:13:44 -0500

    grammar is basic but works

--- fast_test.py
+++ fast_test.py
@@ -1,25 +1,27 @@
 import sys 
-from typing import Dict
+from typing import List
 import logging
 
 
 from lark import Lark, logger
 from lark.exceptions import UnexpectedToken, UnexpectedCharacters
-#from lark.indenter import Indenter
-from PyDayuri.indenter import Indenter,IndentType, IndenterError
+from lark.indenter import Indenter
 
-#logger.setLevel(logging.DEBUG)
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
+#logger.setLevel(logging.INFO)
 
 class TreeIndenter(Indenter):
-    REGULAR_INDENT = ("_INDENT", "_DEDENT", "_NL")
-    INDENT_BLOCKS = {"LET":(IndentType.at_next, "IN", "_LET_SEPARATOR")}
-    PRODUCE_INDENT:Dict[str, IndentType] = {"IN":IndentType.at_next}
+    NL_type = '_NL'
+    OPEN_PAREN_types : List[str]= []
+    CLOSE_PAREN_types : List[str] = []
+    INDENT_type = '_INDENT'
+    DEDENT_type = '_DEDENT'
+    tab_len = 4
 
+with open("PyDayuri/grammar.lark") as grammar:
+  p = Lark(grammar,parser="lalr", postlex=TreeIndenter(), debug=True)
 
-with open("examples/indenter.lark") as f:
-  p = Lark(f,parser="lalr", lexer="standard", postlex=TreeIndenter(), debug=True)
-
+"""
 with open(sys.argv[1]) as f :
   f2 = f.read()
   try :
@@ -27,13 +29,10 @@ with open(sys.argv[1]) as f :
     print(f2)
     print()
     print(parsed.pretty())
-  except IndenterError as e:
-      print(e.pre_msg)
-      print(UnexpectedToken.get_context(e.token,f2))
-      print(e.post_msg)
   except UnexpectedToken as e:
       print(e.get_context(f2))
       print(e)
   except UnexpectedCharacters as e:
       print(e)
+"""
 
